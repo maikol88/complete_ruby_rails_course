02bfd65623f8295f3096fef359913686b8b3fe34
Miguel Saldana <mig.saldana@gmail.com>
2017-01-07 19:55:24 -0600

    Added before action for articles controller on rails alpha blog

--- rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+
   def new
     @article = Article.new
   end
@@ -15,7 +18,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -23,11 +25,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
 
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
@@ -38,13 +38,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
