4e313a1bed83d51bc4aa1cec3a75a667ecd7832a
Miguel Saldana <mig.saldana@gmail.com>
2017-01-08 23:09:47 -0600

    refactored flash messages

--- rails_projects/test_app/app/controllers/articles_controller.rb
+++ rails_projects/test_app/app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
+    obj = Article.new
   end
 
   # GET /articles/1/edit
@@ -24,15 +24,15 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    obj = Article.new(article_params)
 
     respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+      if obj.save
+        format.html { redirect_to obj, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: obj }
       else
         format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json { render json: obj.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,12 +41,12 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1.json
   def update
     respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+      if obj.update(article_params)
+        format.html { redirect_to obj, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: obj }
       else
         format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json { render json: obj.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    @article.destroy
+    obj.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
       format.json { head :no_content }
@@ -64,11 +64,11 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      obj = Article.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:obj).permit(:title, :description)
     end
 end
--- rails_projects/test_app/test/controllers/articles_controller_test.rb
+++ rails_projects/test_app/test/controllers/articles_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @article = articles(:one)
+    obj = articles(:one)
   end
 
   test "should get index" do
@@ -17,30 +17,30 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { description: @article.description, title: @article.title } }
+      post articles_url, params: {obj: {description: obj.description, title: obj.title } }
     end
 
     assert_redirected_to article_url(Article.last)
   end
 
   test "should show article" do
-    get article_url(@article)
+    get article_url(obj)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_article_url(@article)
+    get edit_article_url(obj)
     assert_response :success
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { description: @article.description, title: @article.title } }
-    assert_redirected_to article_url(@article)
+    patch article_url(obj), params: {obj: {description: obj.description, title: obj.title } }
+    assert_redirected_to article_url(obj)
   end
 
   test "should destroy article" do
     assert_difference('Article.count', -1) do
-      delete article_url(@article)
+      delete article_url(obj)
     end
 
     assert_redirected_to articles_url
